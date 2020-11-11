d8f1983915db125f0890d92a4e1df54300cd5585
Marcelo Coelho <marcelo.coelho@softvision.com.br>
2020-11-11 11:56:37 -0300

    c1 - Adicionando arquivos HTML, CSS, Javascript e JSON

---
+++ js/main.js
@@ -0,0 +1,82 @@
+function inicializar() {
+  document.getElementById("txtVlrCobranca").value = '';
+  document.getElementById("txtVlrCobranca").focus();
+  document.getElementById("txtVlrBoleto").value = '0,00';
+  document.getElementById("fraVlrBoleto").style.display = 'none';
+  document.getElementById("txtVlrNF").value = '0,00';
+  document.getElementById("fraVlrNF").style.display = 'none';
+  document.getElementById("txtVlrINSS").value = '11,00';
+  document.getElementById("txtVlrISSQN").value = '2,01';
+}
+
+function calcular() {
+  var objErro = JSON.parse(validar());
+
+  if (objErro.Numero != "0") {
+    alert(objErro.Descricao);
+    document.getElementById(objErro.Objeto).focus();
+    return;
+  }
+
+  document.getElementById("txtVlrBoleto").value = '12,34';
+  document.getElementById("txtVlrNF").value = '56,78';
+  document.getElementById("fraVlrBoleto").style.display = 'block';
+  document.getElementById("fraVlrNF").style.display = 'block';
+  document.getElementById("txtVlrCobranca").focus();
+}
+
+function validar() {
+  var objCampo = document.getElementById("txtVlrCobranca");
+  if (objCampo.value == '') {
+    return '{ "Numero":"1" , "Descricao":"O campo valor da cobrança deve ser preenchido." , "Objeto":"txtVlrCobranca" }';
+  }
+
+  var objErro = JSON.parse(validarFormatoMoeda(objCampo.value));
+  switch (objErro.Numero) {
+    case "1":
+      return '{ "Numero":"2" , "Descricao":"O campo valor da cobrança deve ser numérico." , "Objeto":"txtVlrCobranca" }';
+
+    case "2":
+    case "3":
+      return '{ "Numero":"3" , "Descricao":"O campo valor da cobrança não está formatado corretamente." , "Objeto":"txtVlrCobranca" }';
+
+    default:
+      return '{ "Numero":"0" , "Descricao":"Não foram detectadas inconsistências." , "Objeto":"Null" }';
+  }
+}
+
+function validarFormatoMoeda(pCampo) {
+  var strCaracter;
+  var strJSON;
+  var intCont;
+  var qtdDigitos = 0;
+  var qtdPontos = 0;
+  var qtdVirgulas = 0;
+  var qtdOutros = 0;
+
+  for (intCont = 0; intCont < pCampo.length; intCont++) {
+    strCaracter = pCampo.substr(intCont, 1);
+    if (strCaracter == '0' || strCaracter == '1' || strCaracter == '2' || strCaracter == '3' || strCaracter == '4' ||
+        strCaracter == '5' || strCaracter == '6' || strCaracter == '7' || strCaracter == '8' || strCaracter == '9') {
+      qtdDigitos++;
+    } else if (strCaracter == '.') {
+        qtdPontos++;
+    } else if (strCaracter == ',') {
+        qtdVirgulas++;
+    } else {
+        qtdOutros++;
+    }
+  }
+
+  if (qtdOutros > 0) {
+    strJSON = '{ "Numero":"1" , "Descricao":"Caracteres não numéricos" }';
+  } else if (qtdVirgulas > 1) {
+      strJSON = '{ "Numero":"2" , "Descricao":"Vírgulas" }';
+  } else if (qtdPontos > 0) {
+      strJSON = '{ "Numero":"3" , "Descricao":"Pontos" }';
+  } else {
+      strJSON = '{ "Numero":"0" , "Descricao":"Não há erros de formatação" }';
+  }
+
+  return strJSON;
+}
